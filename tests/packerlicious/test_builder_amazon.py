c07a4695d1afc024bf2a0a7b2f747024f2974420
Blazej Kwasniak <kwasniakblazej@gmail.com>
2017-10-06 17:52:56 +0200

    Squashed commit of the following:
    commit 15d446ad44fff2176028522a93f3c3cc77dd1c6a
    Author: Blazej Kwasniak <kwasniakblazej@gmail.com>
    Date:   Thu Oct 5 23:50:24 2017 +0200
    
        Adding support for AmazonEbsVolume
    
        based on 'https://www.packer.io/docs/builders/amazon-ebsvolume.html'
    
        Closes #16
    
    commit 630654b3dd72282d34ec77de37cf62de1e7347d0
    Author: Blazej Kwasniak <kwasniakblazej@gmail.com>
    Date:   Thu Oct 5 23:08:20 2017 +0200
    
        Adding support for AmazonEbsSurrogate
    
        based on 'https://www.packer.io/docs/builders/amazon-ebssurrogate.html'
    
        Resolves 2/3 of #16
    
    commit 2dcc4cee6517793fee549a731f04a166a00f737c
    Author: Blazej Kwasniak <kwasniakblazej@gmail.com>
    Date:   Thu Oct 5 22:27:52 2017 +0200
    
        Adding support for AmazonChroot
    
        based on 'https://www.packer.io/docs/builders/amazon-chroot.html'
    
        Resolves 1/3 of issue #16

--- tests/packerlicious/test_builder_amazon.py
+++ tests/packerlicious/test_builder_amazon.py
@@ -87,3 +87,101 @@ class TestAmazonEbsBuilder(object):
         with pytest.raises(ValueError) as excinfo:
             b.to_dict()
         assert 'AmazonEbs: only one of the following can be specified: security_group_id, security_group_ids' == str(excinfo.value)
+
+class TestAmazonChroot(object):
+
+    def test_required_fields_missing(self):
+        b = builder.AmazonChroot()
+
+        with pytest.raises(ValueError) as excinfo:
+            b.to_dict()
+        assert 'required' in str(excinfo.value)
+
+    def test_exactly_one_source_ami(self):
+        b = builder.AmazonChroot(
+            ami_name  ="some-ami",
+            access_key="dummy-access-key",
+            secret_key="dummy-secret-key",
+        )
+
+        with pytest.raises(ValueError) as excinfo:
+            b.to_dict()
+        assert 'AmazonChroot: one of the following must be specified: source_ami, source_ami_filter' == str(excinfo.value)
+
+class TestAmazonEbsSurrogate(object):
+
+    def test_required_fields_missing(self):
+        b = builder.AmazonEbsSurrogate()
+
+        with pytest.raises(ValueError) as excinfo:
+            b.to_dict()
+        assert 'required' in str(excinfo.value)
+
+    def test_exactly_one_source_ami(self):
+        ami_root_dev = builder.BlockDeviceMapping()
+        b = builder.AmazonEbsSurrogate(
+            access_key="dummy-access-key",
+            secret_key="dummy-secret-key",
+            instance_type="t2.micro",
+            source_device_name="some_device",
+            region="us-east-1",
+            ami_root_device=[ami_root_dev],
+        )
+
+        with pytest.raises(ValueError) as excinfo:
+            b.to_dict()
+        assert 'AmazonEbsSurrogate: one of the following must be specified: source_ami, source_ami_filter' == str(excinfo.value)
+
+    def test_mutually_exclusive_security_group_ami(self):
+        ami_root_dev = builder.BlockDeviceMapping()
+        b = builder.AmazonEbsSurrogate(
+            access_key="dummy-access-key",
+            secret_key="dummy-secret-key",
+            instance_type="t2.micro",
+            source_device_name="some_device",
+            region="us-east-1",
+            ami_root_device=[ami_root_dev],
+            source_ami="dummy-source-ami",
+            security_group_id="sg-123",
+            security_group_ids=["sg-123", "sg-456"],
+        )
+
+        with pytest.raises(ValueError) as excinfo:
+            b.to_dict()
+        assert 'AmazonEbsSurrogate: only one of the following can be specified: security_group_id, security_group_ids' == str(excinfo.value)
+
+class TestAmazonEbsVolume(object):
+
+    def test_required_fields_missing(self):
+        b = builder.AmazonEbsVolume()
+
+        with pytest.raises(ValueError) as excinfo:
+            b.to_dict()
+        assert 'required' in str(excinfo.value)
+
+    def test_exactly_one_source_ami(self):
+        b = builder.AmazonEbsVolume(
+            access_key="dummy-access-key",
+            secret_key="dummy-secret-key",
+            instance_type="t2.micro",
+            region="us-east-1",
+        )
+
+        with pytest.raises(ValueError) as excinfo:
+            b.to_dict()
+        assert 'AmazonEbsVolume: one of the following must be specified: source_ami, source_ami_filter' == str(excinfo.value)
+
+    def test_mutually_exclusive_security_group_ami(self):
+        b = builder.AmazonEbsVolume(
+            access_key="dummy-access-key",
+            secret_key="dummy-secret-key",
+            instance_type="t2.micro",
+            region="us-east-1",
+            source_ami="dummy-source-ami",
+            security_group_id="sg-123",
+            security_group_ids=["sg-123", "sg-456"],
+        )
+
+        with pytest.raises(ValueError) as excinfo:
+            b.to_dict()
+        assert 'AmazonEbsVolume: only one of the following can be specified: security_group_id, security_group_ids' == str(excinfo.value)
