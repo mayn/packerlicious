df2e1af6d83ea71e2271c901a5b4382bcc5835a8
Sivadas, Sibidas <Sibidas_Sivadas@intuit.com>
2018-10-19 01:27:59 +0530

    correcting the clone_from_vmcx_path typo in tests assertion string

--- tests/packerlicious/test_builder_hyperv.py
+++ tests/packerlicious/test_builder_hyperv.py
@@ -20,7 +20,7 @@ class TestHypervVmcxBuilder(object):
 
         with pytest.raises(ValueError) as excinfo:
             b.to_dict()
-        assert 'HypervVmcx: one of the following must be specified: clone_from_vmxc_path, clone_from_vm_name' == str(excinfo.value)
+        assert 'HypervVmcx: one of the following must be specified: clone_from_vmcx_path, clone_from_vm_name' == str(excinfo.value)
 
     def test_exactly_one_clone_from_required(self):
         b = builder.HypervVmcx(
@@ -30,7 +30,7 @@ class TestHypervVmcxBuilder(object):
 
         with pytest.raises(ValueError) as excinfo:
             b.to_dict()
-        assert 'HypervVmcx: only one of the following can be specified: clone_from_vmxc_path, clone_from_vm_name' == str(
+        assert 'HypervVmcx: only one of the following can be specified: clone_from_vmcx_path, clone_from_vm_name' == str(
             excinfo.value)
 
     def test_exactly_one_clone_from_specified(self):
